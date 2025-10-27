8857b72e439a32f079c8e98d803bd280dcfe6df8
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-10-27 11:56:28 +0530

    Worked on Admin Portal

---
+++ client/src/pages/admin/ListBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListBlog = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1>List of Blogs</h1>
+    </div>
+  )
+}
+
+export default ListBlog
