20db00d979dd5d2b948beac8c35d7dbf895b289b
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-10-27 23:40:04 +0530

    Middlewares and BlogRoutes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("✅ API is running!");
 });
 app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
 // 404 Handler
 app.use((req, res) => {
